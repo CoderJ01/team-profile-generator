68b32ed36df5dc9577af354fee7b8748fbb30ccd
Coder J <buildingguy@sbcglobal.net>
2022-01-24 20:27:04 -0800

    Testing123: try out different test set ups

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,11 +2,15 @@ const Employee = require('../lib/Employee.js');
 
 describe('Employee', () => {
     describe('name', () => {
-        it('should return a name', () => {
+        it('should return a string', () => {
 
-            // more code...
-         
+            const usersName = new Employee('Jason'); // terminal: Expected Any<String>
+            // const usersName = new Employee().getName('Jason'); //terminal: Expected "Jason"
+            // expect(usersName).toBe('Jason'); // Expected: "Jason", Received: {"email": "", "id": "", "name": "Jason"}
+            // expect(name).toBe('Jason'); // ReferenceError: name is not defined
+            expect(usersName.name).toBe('Jason'); // Expected: Any<String>, Received: undefined
+            expect(usersName.name).toEqual(expect.any(String));
+
         });
     });
-
 });
