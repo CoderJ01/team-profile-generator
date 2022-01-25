6d329fca439516049bff94a3c446d25d19f9913c
Coder J <buildingguy@sbcglobal.net>
2022-01-24 20:27:40 -0800

    clean up (comments)

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,11 +4,8 @@ describe('Employee', () => {
     describe('name', () => {
         it('should return a string', () => {
 
-            const usersName = new Employee('Jason'); // terminal: Expected Any<String>
-            // const usersName = new Employee().getName('Jason'); //terminal: Expected "Jason"
-            // expect(usersName).toBe('Jason'); // Expected: "Jason", Received: {"email": "", "id": "", "name": "Jason"}
-            // expect(name).toBe('Jason'); // ReferenceError: name is not defined
-            expect(usersName.name).toBe('Jason'); // Expected: Any<String>, Received: undefined
+            const usersName = new Employee('Jason');
+            expect(usersName.name).toBe('Jason'); 
             expect(usersName.name).toEqual(expect.any(String));
 
         });
